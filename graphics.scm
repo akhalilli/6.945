0774d68612db5d2cafd110e359aba011c5da3e42
Brian Shimanuki <bshimanuki@mit.edu>
2016-05-09 01:45:04 -0400

    random walk graphics

--- graphics.scm
+++ graphics.scm
@@ -26,6 +26,11 @@
 (define (make-graphics)
   (make-graphics-device (car (enumerate-graphics-types))))
 
+(define (graphics-draw-dot device x y)
+  (for-each (lambda (c)
+              (graphics-draw-text device x y (symbol->string c)))
+            '(a e g i j p q s t u v w x y z)))
+
 (define (draw-graph vertex proj #!optional device)
   (let ((device (if (default-object? device) (make-graphics) device)))
     ((graph-dfs (lambda (v)
@@ -49,6 +54,56 @@
         (sleep-current-thread 50)
         (iter (+ t rate))))))
 
+(define (draw-random-walk vertex proj #!optional sleep scale device)
+  (let ((device (if (default-object? device) (make-graphics) device))
+        (scale (if (default-object? scale) 1 scale))
+        (sleep (if (default-object? sleep) 50 sleep))
+        (vertices (make-eq-hash-table))
+        (edges (make-equal-hash-table)))
+    (define (hash-table/get-edge edge)
+      (or (hash-table/get edges (list (edge-tail edge) (edge-head edge)) #f)
+          (hash-table/get edges (list (edge-head edge) (edge-tail edge)) #f)))
+    (define (hash-table/put-edge! edge val)
+      (hash-table/put! edges (list (edge-tail edge) (edge-head edge)) val))
+    (graphics-set-coordinate-limits device (- scale) (- scale) scale scale)
+    (hash-table/put! vertices vertex 0)
+    (let iter ((vertex vertex)
+               (t 0))
+      (graphics-clear device)
+      (apply graphics-draw-dot (cons device (proj vertex)))
+      ((graph-dfs
+         (lambda (v)
+           (let ((time (hash-table/get vertices v #f)))
+             (if time
+             ; (apply graphics-draw-dot (cons device (proj v))))
+             (apply graphics-draw-text (append (cons device (proj v)) `(,(string time))))))
+           (stream-for-each
+             (lambda (edge)
+               (graphics-bind-line-style
+                 device
+                 (if (hash-table/get-edge edge)
+                   0  ; solid
+                   1) ; dash
+                 (lambda ()
+                   (apply graphics-draw-line
+                          (cons device (append (proj (edge-tail edge))
+                                               (proj (edge-head edge))))))))
+             (vertex-edges-stream v))
+           0))
+       vertex)
+      (sleep-current-thread sleep)
+      (let* ((edges-stream (vertex-edges-stream vertex))
+            (edge (if (null? edges-stream)
+                    #f
+                    (stream-ref edges-stream (random (stream-length edges-stream)))))
+            (next (if edge (edge-head edge) vertex)))
+        (if (not (hash-table/get vertices vertex #f))
+          (hash-table/put! vertices vertex t))
+        (if (and edge
+                 (not (hash-table/get-edge edge)))
+          (hash-table/put-edge! edge t))
+        (iter next (1+ t))))))
+
 #|
 (draw-graph tetrahedron (camera-vertex '(0 3 -1) '(9 1 2) 0.5))
 (draw-graph octahedron (camera-vertex '(0 0 -1) '(0 1 0)))
@@ -66,4 +121,8 @@
 (draw-graph (make-complete 12) circle-plot)
 (draw-graph ((make-di-erdos 6 0.5) 0) circle-plot)
 (draw-graph ((make-di-erdos 12 0.5) 0) circle-plot)
+|#
+
+#|
+(draw-random-walk (make-circle 12) circle-plot 200 1.2)
 |#
